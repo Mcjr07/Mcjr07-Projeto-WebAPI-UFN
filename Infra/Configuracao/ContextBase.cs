a3cb848349cd6572145168984030fa52c0162fa2
Mcjr07 <https://github.com/Mcjr07/Desafios-Academia-UFN-ATS.git>
2024-01-02 14:14:53 -0300

    Atulização JW e a criação de nova migration.

---
+++ Infra/Configuracao/ContextBase.cs
@@ -0,0 +1,50 @@
+﻿using Entities.Entidades;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Configuracao
+{
+    public class ContextBase : IdentityDbContext<ApplicationUser>
+    {
+        public ContextBase( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<SistemaFinanceiro> SistemaFinanceiro { set; get; }
+        public DbSet<UsuarioSistemaFinanceiro> UsuarioSistemaFinanceiro { set; get; }
+        public DbSet<Categoria> Categoria { set; get; }
+        public DbSet<Despesa> Despesa { set; get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ObterStringConexao());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUsers").HasKey(t => t.Id);
+
+            base.OnModelCreating(builder);
+        }
+
+
+        public string ObterStringConexao()
+        {
+             return "Data Source=MARCELUS-23\\SQLSERVER2023;Initial Catalog=Financeiro;Integrated Security=False;User ID=sa;Password=789532;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False";
+        
+        }
+
+
+    }
+}
